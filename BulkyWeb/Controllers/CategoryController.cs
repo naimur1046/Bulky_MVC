fd6cf329fb0bf800f674404f342beb9c3b2cb56b
naimur rahman rumel <naimurrahman1046@gmail.com>
2024-10-17 07:36:09 +0600

    Here I have implemented the Delete feature fully

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Controllers
 {
@@ -61,10 +62,7 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Edit(Category category)
         {
-            if (category.Name.ToString() == category.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "Here two field has same value");
-            }
+            
 
             if (ModelState.IsValid)
             {
@@ -73,6 +71,42 @@ namespace BulkyWeb.Controllers
                 return RedirectToAction("Index", "Category");
             }
             return View();
+
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+
+            Category categoryFromDb = _db.Categories.Find(Id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == Id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==Id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound(categoryFromDb);
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? Id)
+        {
+
+            Category? category = _db.Categories.Find(Id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+            
 
         }
     }
